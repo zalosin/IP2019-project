39d8daa210c7e891c457b7852f20e1933a111621
Andreyutul3000 <34367689+Andreyutul3000@users.noreply.github.com>
2019-05-23 00:33:57 +0300

    Search finished

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let url = "https://cors-anywhere.herokuapp.com/http://newsbagserver.herokuapp.com/article/all";
 
 function getArticles() {
-
+    
     fetch(url)
         .then((response) => {
             if (response.ok) {
@@ -128,3 +128,51 @@ function search(elem){
         window.location.replace("search-results.html?search="+elem.value);
     }
 }
+
+function getSearchedArticles(elem) {
+    elem = elem.toLowerCase();
+    fetch(url)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then((json) => {
+            let articles = json;
+            for (i = 0; i < articles.length; i++){
+                if((!articles[i].title.toLowerCase().includes(elem)) && (!articles[i].body.toLowerCase().includes(elem))){
+                    articles.splice(i, 1);
+                    i = i - 1;                    
+                }
+            }
+            if ( window.location.pathname.indexOf("search-results.html") > -1 ){
+                renderSearched(articles);
+            }
+        })
+}
+
+function renderSearched(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        var rndStory = Math.floor(Math.random() * arr.length);
+        var date = new Date(0);
+        date.setUTCSeconds(arr[i].createTime);
+        var date_string = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+
+        let el1 = `<div id="mostRead-"` + i + ` class="col-md-12">
+                        <div class="post post-row">
+                            <a class="post-img" href="blog-post.html"><img src="./img/post-4.jpg" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-meta">
+                                    <a class="post-category cat-2" href="category.html">JavaScript</a>
+                                    <span class="post-date">` + date_string + `</span>
+                                </div>
+                                <h3 class="post-title"><a href="blog-post.html">` + arr[rndStory].title + `</a></h3>
+                                <p class="truncate" id="body">` + arr[rndStory].body.substr(0, 100) + "..." + `</p>
+                            </div>
+                        </div>
+                    </div>`
+        $('#search-result-div').append(el1);
+    }
+}
+
+getArticles();
